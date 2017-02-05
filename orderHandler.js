82d7b1a39b3bdc9ce4d1152af36642597a0492d3
hmgomez <harvey@1and0.co.uk>
2017-02-05 02:16:29 +0000

    Check if JSON has the right keys in the response

--- orderHandler.js
+++ orderHandler.js
@@ -1,19 +1,20 @@
 function sumOrders(orderbook) {
     orderbook.bidSum = 0;
     orderbook.askSum = 0;
-    var bidSum = 0;
-    var askSum = 0;
 
-    orderbook.bids.forEach(function(bid) {
-        orderbook.bidSum += bid[0] * bid[1];
-    });
+    if ("bids" in orderbook) {
+        orderbook["bids"].forEach(function(bid) {
+            orderbook.bidSum += bid[0] * bid[1];
+        });
+    }
 
-    orderbook.asks.forEach(function(ask) {
-        orderbook.askSum += ask[1];
-    });
+    if ("asks" in orderbook) {
+        orderbook["asks"].forEach(function(ask) {
+            orderbook.askSum += ask[1];
+        });
+    }
 
     return orderbook;
-
 }
 
 module.exports = {
